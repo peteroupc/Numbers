bf8c0828a1db19b7c66eff799f007c04ac96ce21
NZSmartie <nzsmartie@gmail.com>
2017-07-17 07:40:32 +1200

    Recreated Numbers.csproj to support .Net Standard and new Nuget

---
+++ Numbers/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+/*
+Written by Peter O. in 2013.
+Any copyright is dedicated to the Public Domain.
+http://creativecommons.org/publicdomain/zero/1.0/
+If you like this, you should donate to Peter O.
+at: http://peteroupc.github.io/
+ */
+#region Using directives
+using System;
+using System.Reflection;
+#endregion
+[assembly: CLSCompliant(true)]
