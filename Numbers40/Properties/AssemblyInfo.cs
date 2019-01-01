35789aeb81a8bdacf26be2f82c387ff5329c7970
Peter O <poccil14@gmail.com>
2019-01-01 05:57:00 -0500

    Version 1.2

--- Numbers40/Properties/AssemblyInfo.cs
+++ Numbers40/Properties/AssemblyInfo.cs
@@ -1,9 +1,8 @@
 using System.Reflection;
-
 [assembly: System.CLSCompliant(true)]
-[assembly: AssemblyInformationalVersion("1.1.2")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.2.0")]
+[assembly: AssemblyInformationalVersion("1.2")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0")]
 [assembly: AssemblyProduct("Arbitrary-Precision Number Library")]
 [assembly: AssemblyTitle("Arbitrary-Precision Number Library")]
 [assembly: AssemblyDescription("A C# library that supports arbitrary-pre" +
