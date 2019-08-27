8b38a2cdd16b21a7b4704b7183c99179d98de871
Peter O <poccil14@gmail.com>
2019-08-27 19:19:06 -0400

    Nonsubstantive code edits; etc.

--- Numbers/PeterO/Numbers/ETrapException.cs
+++ Numbers/PeterO/Numbers/ETrapException.cs
@@ -87,6 +87,7 @@ public sealed class ETrapException : ArithmeticException {
     /// object.</param>
     /// <param name='context'>A
     /// System.Runtime.Serialization.StreamingContext object.</param>
+    ///
       protected ETrapException(
         System.Runtime.Serialization.SerializationInfo info,
         System.Runtime.Serialization.StreamingContext context)
